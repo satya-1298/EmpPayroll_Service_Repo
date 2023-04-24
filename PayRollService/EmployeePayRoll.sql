45a4d93599260922768e5e76fb76f31fce2514ec
unknown <satya-1298>
2023-04-24 19:54:02 +0530

    Creating table employee

---
+++ PayRollService/EmployeePayRoll.sql
@@ -0,0 +1,19 @@
+/****************UC2***********/
+create table Employee
+(
+Id int Identity,
+Name varchar(200),
+Salary varchar(20),
+Start_Date varchar(20)
+)
+ select*from Employee
+ /*************UC3************/
+ insert into Employee values('Malik','30000','2020-12-20')
+ insert into Employee values('Rosey','25000','2019-05-10')
+ insert into Employee values('divya','43000','2018-08-06')
+ insert into Employee values('Akki','20000','2020-03-28')
+ insert into Employee values('jack','35000','2018-02-20')
+
+ 
+ 
+ 
